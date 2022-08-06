83e05044604ff9d31be3159ff7df709e660840fa
Aaron Deas <deas.aaron@gmail.com>
2022-08-06 17:39:30 -0400

    Saving progress

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -3,28 +3,38 @@ const nameInput = document.getElementById('nameinputfield');
 const homeBody = document.getElementById('homebody');
 const userLocal = localStorage.getItem('userData') || null;
 const nameForm = document.getElementById('nameentryform');
-
-const displayBpForm = () => {
-  homeBody.appendChild(document.createElement('div'));
-  homeBody.appendChild(document.createElement('form'));
-};
+const bpTrackerForm = document.getElementById('bptrackerform');
+const entrySaveBtn = document.getElementById('entrysavebutton');
+const systolic = document.getElementById('systolicVal');
+const diastolic = document.getElementById('diastolicVal');
+const entryDate = document.getElementById('entrydate');
 
 if (userLocal) {
   let usrName = JSON.parse(localStorage.getItem('userData')).name;
   nameColumn.innerText = `${usrName}'s bloodpressure tracker.`;
+  bpTrackerForm.style.visibility = 'visible';
   nameForm.style.visibility = 'hidden';
-  displayBpForm();
 } else {
-  console.log('no name');
+  bpTrackerForm.style.visibility = 'hidden';
   nameColumn.innerText = 'Please enter name:';
 }
+
+entrySaveBtn.onclick = (e) => {
+  e.preventDefault();
+  userData.entrydate = entryDate.value;
+  userData.systolic = systolic.value;
+  userData.diastolic = diastolic.value;
+  saveToLocal();
+};
 
 const submitFunction = (e) => {
   let userData = {};
   e.preventDefault();
-  nameColumn.innerText = `${nameInput.value}'s bloodpressure tracker:`;
+  nameColumn.innerHTML = `<h2>${nameInput.value}'s bloodpressure tracker:</h2>`;
   userData.name = nameInput.value.trim();
   saveToLocal(userData);
+  nameForm.style.visibility = 'hidden';
+  bpTrackerForm.style.visibility = 'visible';
 };
 // function for saving data to localstorage.
 const saveToLocal = (usrData) => {
