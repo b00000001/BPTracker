f88c5bc5ee1a9dc4f5f939c66ac3c0eddf492d72
Aaron Deas <deas.aaron@gmail.com>
2022-08-06 03:39:20 -0400

    Saving progress

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,7 +1,43 @@
-const testFunction = () => {
-    console.log('Test Function');
+const nameColumn = document.getElementById('namecolumn');
+const nameInput = document.getElementById('nameinputfield');
+const homeBody = document.getElementById('homebody');
+const userLocal = localStorage.getItem('userData') || null;
+const nameForm = document.getElementById('nameentryform');
+
+const displayBpForm = () => {
+  homeBody.appendChild(document.createElement('div'));
+  homeBody.appendChild(document.createElement('form'));
+};
+
+if (userLocal) {
+  let usrName = JSON.parse(localStorage.getItem('userData')).name;
+  nameColumn.innerText = `${usrName}'s bloodpressure tracker.`;
+  nameForm.style.visibility = 'hidden';
+  displayBpForm();
+} else {
+  console.log('no name');
+  nameColumn.innerText = 'Please enter name:';
 }
 
-const submitFunction = () => {
-    console.log('Submit Function')
-}
+const submitFunction = (e) => {
+  let userData = {};
+  e.preventDefault();
+  nameColumn.innerText = `${nameInput.value}'s bloodpressure tracker:`;
+  userData.name = nameInput.value.trim();
+  saveToLocal(userData);
+};
+// function for saving data to localstorage.
+const saveToLocal = (usrData) => {
+  localStorage.setItem('userData', JSON.stringify(usrData));
+  console.log(
+    `
+   Saving items to local storage... 
+   Items currently in storage:
+   ${checkLocal()}
+   `
+  );
+};
+
+const checkLocal = () => {
+  return localStorage.getItem('userData');
+};
